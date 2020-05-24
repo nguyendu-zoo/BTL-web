567ef2291ca1457b30069b81e179229b1c1d9d53
DU <nguyenduhaihauc@gmail.com>
2020-05-24 15:55:37 +0700

    DU:content nen co ban

---
+++ code/content.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <title>content</title>
+    <link rel="stylesheet" href="style.css">
+    <script></script>
+</head>
+<body>
+    <div class="content">
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-black-lie.jpg" alt="" class="img1">
+            <img src="V-black-stand.jpg" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-while-red.jpg" alt="" class="img">
+            <img src="V-while-red-lie.jpg" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+        <div class="product">
+            <img src="V-gray-lie.png" alt="" class="img1">
+            <img src="V-gray-stand.png" alt="" class="img2">
+            <div class="layer3">
+                <div class="lrleft"><a href="">Van old</a></div>
+                <div class="lrright">25$</div>
+            </div>
+            <div class="layer4">The Men</div>
+            <div class="layer5"><button>Thêm vào giỏ</button> </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ code/desgin.php
@@ -0,0 +1,21 @@
+<table  width="100%" height = "100%">
+	<tr>
+		<td>
+			Logo
+		</td>
+		<td colspan="2">
+			Banner.php
+		</td>
+		<?php 
+			/*include("banner.php");*/
+		?>
+	</tr>
+	<tr>
+		<td rowspan="2">ml.php</td>
+		<td>content.php</td>
+		<td rowspan="2">mr.php</td>
+	</tr>
+	<tr>
+		<!-- requie -->
+	</tr>
+</table>
